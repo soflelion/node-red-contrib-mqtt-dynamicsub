e9b6254ede9f2a898a022cf52b3e2cace35b562e
Crowbar Z <crowbarz@outlook.com>
2020-08-12 16:51:23 +0100

    Updated to support multiple topics
    - support multiple simultaneously subscribed topics
    - support unsubscribe of individual or all topics
    - show subscribed topics on node context
    - documentation updates

--- mqtt.js
+++ mqtt.js
@@ -27,11 +27,12 @@ module.exports = function (RED) {
         if (isNaN(this.qos) || this.qos < 0 || this.qos > 2) {
             this.qos = 2;
         }
-	this.name = n.name;
+        this.name = n.name;
         this.broker = n.broker;
         this.brokerConn = RED.nodes.getNode(this.broker);
         this.datatype = n.datatype || "utf8";
         this.unsubscribeAfterFirstMsgRecv = n["unsubscribe-after-first-msg-recv"];
+        this.subscribed_topics = {};
         var node = this;
         if (this.brokerConn) {
             this.status({
@@ -40,37 +41,83 @@ module.exports = function (RED) {
                 text: "node-red:common.status.disconnected"
             });
             this.on("input", function (msg) {
-                this.topic = msg.topic;
-                this.payload = msg.payload;
-                if (this.topic) {
-                    if (!/^(#$|(\+|[^+#]*)(\/(\+|[^+#]*))*(\/(\+|#|[^+#]*))?$)/.test(this.topic)) {
-                        return this.warn(RED._("mqtt.errors.invalid-topic"));
+                var topic = msg.topic;
+                // this.payload = msg.payload;
+                if (typeof(msg.unsubscribe) !== 'undefined') {
+                    // unsubscribe property specified, process
+                    var unsubscribe = msg.unsubscribe;
+                    if (unsubscribe === true && node.subscribed_topics[topic]) {
+                        // unsubscribe single topic
+                        this.warn("msg.unsubscribe = \"" + topic + "\"");
+                        this.brokerConn.unsubscribe(topic, node.id, true);
+                        delete node.subscribed_topics[topic]
+                        this.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                        return;
+                    } else if (unsubscribe === "all") {
+                        // unsubscribe all topics
+                        this.warn("msg.unsubscribe = \"all\"");
+                        for (topic in node.subscribed_topics) {
+                            this.brokerConn.unsubscribe(topic, node.id, true);
+                            delete node.subscribed_topics[topic]
+                            this.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                        }
+                        return;
+                    } else {
+                        // topic not subscribed
+                        this.error(RED._("mqtt.errors.not-subscribed"));
+                    }
+                }
+                if (topic) {
+                    if (!/^(#$|(\+|[^+#]*)(\/(\+|[^+#]*))*(\/(\+|#|[^+#]*))?$)/.test(topic)) {
+                        node.warn(RED._("mqtt.errors.invalid-topic"));
+                        return;
                     }
                     node.brokerConn.register(this);
-                    this.brokerConn.subscribe(this.topic, this.qos, function (topic, payload, packet) {
-			// Unsubscribe string format "_unsubscribe:<nodeName>"
-			var unsubNode = payload.toString().split(":");
-			unsubNode = unsubNode.length === 2 && unsubNode[0] === "_unsubscribe" ? unsubNode[1] : null;
-			// Check if unsubscribe action is related to this specific node
-			if (unsubNode === node.name) {
-			    node.brokerConn.unsubscribe(topic,node.id,true);
-			    return;
-			}
-			// Don't pass unsubscribe msg through nodes
-			if (unsubNode) return;
-                        if (node.datatype === "buffer") {
-                            // payload = payload;
-                        } else if (node.datatype === "base64") {
-                            payload = payload.toString('base64');
-                        } else if (node.datatype === "utf8") {
-                            payload = payload.toString('utf8');
-                        } else if (node.datatype === "json") {			    
-                            if (isUtf8(payload)) {
-                                payload = payload.toString();
-                                try {
-                                    payload = JSON.parse(payload);
-                                } catch (e) {
-                                    node.error(RED._("mqtt.errors.invalid-json-parse"), {
+                    this.warn("msg.subscribe = \"" + topic + "\"");
+                    if (! node.subscribed_topics[topic]) {
+                        // Register topic and export to node context
+                        node.subscribed_topics[topic] = Date.now();
+                        this.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                        this.brokerConn.subscribe(topic, this.qos, function (topic, payload, packet) {
+                            // Unsubscribe string format "_unsubscribe:<nodeName>"
+                            var unsubNode = payload.toString().split(":");
+                            unsubNode = unsubNode.length === 2 && unsubNode[0] === "_unsubscribe" ? unsubNode[1] : null;
+                            // Check if unsubscribe action is related to this specific node
+                            if (unsubNode === node.name) {
+                                if (node.subscribed_topics[topic]) {
+                                    node.brokerConn.unsubscribe(topic, node.id, true);
+                                    delete node.subscribed_topics[topic];
+                                    node.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                                    return;
+                                } else {
+                                    // topic not subscribed
+                                    this.error(RED._("mqtt.errors.not-subscribed"));
+                                }
+                            }
+                            // Don't pass unsubscribe msg through nodes
+                            if (unsubNode) return;
+                            if (node.datatype === "buffer") {
+                                // payload = payload;
+                            } else if (node.datatype === "base64") {
+                                payload = payload.toString('base64');
+                            } else if (node.datatype === "utf8") {
+                                payload = payload.toString('utf8');
+                            } else if (node.datatype === "json") {
+                                if (isUtf8(payload)) {
+                                    payload = payload.toString();
+                                    try {
+                                        payload = JSON.parse(payload);
+                                    } catch (e) {
+                                        node.error(RED._("mqtt.errors.invalid-json-parse"), {
+                                            payload: payload,
+                                            topic: topic,
+                                            qos: packet.qos,
+                                            retain: packet.retain
+                                        });
+                                        return;
+                                    }
+                                } else {
+                                    node.error((RED._("mqtt.errors.invalid-json-string")), {
                                         payload: payload,
                                         topic: topic,
                                         qos: packet.qos,
@@ -79,50 +126,55 @@ module.exports = function (RED) {
                                     return;
                                 }
                             } else {
-                                node.error((RED._("mqtt.errors.invalid-json-string")), {
-                                    payload: payload,
-                                    topic: topic,
-                                    qos: packet.qos,
-                                    retain: packet.retain
-                                });
-                                return;
+                                if (isUtf8(payload)) {
+                                    payload = payload.toString();
+                                }
                             }
-                        } else {
-                            if (isUtf8(payload)) {
-                                payload = payload.toString();
+                            var msg = {
+                                topic: topic,
+                                payload: payload,
+                                qos: packet.qos,
+                                retain: packet.retain,
+                            };
+                            if ((node.brokerConn.broker === "localhost") || (node.brokerConn.broker === "127.0.0.1")) {
+                                msg._topic = topic;
                             }
+                            // Unsubscribe if checkbox set
+                            if (node.unsubscribeAfterFirstMsgRecv) {
+                                node.brokerConn.unsubscribe(topic, node.id, true);
+                                delete node.subscribed_topics[topic];
+                                node.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                            }
+
+                            node.send(msg);
+                        }, this.id);
+                        if (this.brokerConn.connected) {
+                            node.status({
+                                fill: "green",
+                                shape: "dot",
+                                text: "node-red:common.status.connected"
+                            });
                         }
-                        var msg = {
-                            topic: topic,
-                            payload: payload,
-                            qos: packet.qos,
-                            retain: packet.retain,
-                        };
-                        if ((node.brokerConn.broker === "localhost") || (node.brokerConn.broker === "127.0.0.1")) {
-                            msg._topic = topic;
-                        }
-                        // Unsubscribe if checkbox set
-                        if (node.unsubscribeAfterFirstMsgRecv) {
-                            node.brokerConn.unsubscribe(topic, node.id, true);
-                        }
-			
-                    	node.send(msg);
-                    }, this.id);
-                    if (this.brokerConn.connected) {
-                        node.status({
-                            fill: "green",
-                            shape: "dot",
-                            text: "node-red:common.status.connected"
-                        });
-                    }              
+                    } else {
+                        // already subscribed
+                        this.error(RED._("mqtt.errors.already-subscribed"));
+                    }
                 } else {
-                    //this.error(RED._("mqtt.errors.not-defined"));
-		    node.send(msg);
+                    // no topic specified
+                    this.error(RED._("mqtt.errors.not-defined"));
+                    // node.send(msg);
                 }
             });
             this.on('close', function (removed, done) {
                 if (node.brokerConn) {
-                    node.brokerConn.unsubscribe(node.topic, node.id, removed);
+                    //  iterate all subscribed topics and unsubscribe all
+                    this.warn("Unsubscribing on close");
+                    for (var topic in node.subscribed_topics) {
+                        this.brokerConn.unsubscribe(topic, node.id, true);
+                        delete this.subscribed_topics[topic]
+                        this.context().set("subscribed_topics", Object.keys(node.subscribed_topics));
+                    }
+                    // node.brokerConn.unsubscribe(node.topic, node.id, removed);
                     node.brokerConn.deregister(node, done);
                 }
             });
@@ -131,5 +183,4 @@ module.exports = function (RED) {
         }
     }
     RED.nodes.registerType("digitaloak-mqtt-in", digitaloak_MQTTInNode);
-
 };
